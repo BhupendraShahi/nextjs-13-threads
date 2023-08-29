448da6984a25595fff78c02620430b71987aef3e
Bhupendra Singh Shahi <shahi23.bhupendra11@gmail.com>
2023-08-29 11:33:29 +0530

    Update user.model.ts

--- lib/models/user.model.ts
+++ lib/models/user.model.ts
@@ -34,6 +34,6 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-const User = mongoose.models.nextthreadsuser || mongoose.model("nextthreadsuser", userSchema);
+const User = mongoose.model("nextthreadsuser", userSchema);
 
 export default User;
