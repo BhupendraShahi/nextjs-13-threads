4a6ba0a7b9033e2e8de0d067b8804fb710e471a0
Bhupendra Singh Shahi <shahi23.bhupendra11@gmail.com>
2023-08-29 11:45:29 +0530

    Update thread.model.ts

--- lib/models/thread.model.ts
+++ lib/models/thread.model.ts
@@ -29,6 +29,6 @@ const threadSchema = new mongoose.Schema({
   ],
 });
 
-const Thread = mongoose.model("Thread", threadSchema);
+const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
 
 export default Thread;
